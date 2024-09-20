82577c0810657276302526c28704cbed995b85b3
nag <nag@CODEMASTER>
2024-09-20 09:04:52 +0100

    Add project files.

---
+++ Dependency Injection Application/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Dependency_Injection_Application
+{
+    public interface IMessageService
+    {
+        void SendMessage(string message);
+    }
+
+    public class ConsoleMessageService : IMessageService
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Notification 
+    {
+        private readonly IMessageService messageService;
+        public Notification(IMessageService messageService)
+        {
+            this.messageService = messageService;
+        }
+        public void notify(string message)
+        {
+            messageService.SendMessage(message);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceprovider = new ServiceCollection() 
+                .AddSingleton<IMessageService, ConsoleMessageService>()
+                .AddSingleton<Notification>()
+                .BuildServiceProvider();
+
+            var notification = serviceprovider.GetService<Notification>();
+            notification.notify("Notification Message");
+        }
+    }
+}
